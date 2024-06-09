798655eb9c666cba7f56c325eb03847350de11b2
Hokkaidosm <64832302+Hokkaidosm@users.noreply.github.com>
2024-06-09 16:25:37 +0900

    シリーズデータを取得する

--- precure_initial/Program.cs
+++ precure_initial/Program.cs
@@ -30,34 +30,15 @@ namespace precure_initial
             public List<string> PrecureKeys { get; set; }
         }
 
-        /// <summary>
-        /// プリキュアデータ.
-        /// </summary>
-        class Precure
-        {
-            /// <summary>
-            /// プリキュア英語名.
-            /// </summary>
-            public string GirlName { get; set; }
-
-            /// <summary>
-            /// プリキュア日本語名.
-            /// </summary>
-            public string PrecureName { get; set; }
-
-            /// <summary>
-            /// イニシャル.
-            /// </summary>
-            public string Initial { get; set; }
-        }
-
         static void Main(string[] args)
         {
             Dictionary<string, Series> seriesList = LoadSeries();
-            Dictionary<string, Precure> precureList = LoadPrecures();
             foreach (var series in seriesList)
             {
-                OutputPrecureList(series.Value.Name, series.Value.PrecureKeys, precureList);
+                Console.WriteLine($"シリーズキー：{series.Key}");
+                Console.WriteLine($"シリーズ名：{series.Value.Name}");
+                Console.WriteLine($"プリキュアキー：[{string.Join(", ", series.Value.PrecureKeys)}]");
+                Console.WriteLine();
             }
         }
 
@@ -87,97 +68,13 @@ namespace precure_initial
                 {
                     precures.Add((string)precure);
                 }
-                series_directory.Add(series.Key, new Series {
+                series_directory.Add(series.Key, new Series
+                {
                     Name = (string)seriesName,
                     PrecureKeys = precures
                 });
             }
             return series_directory;
-        }
-
-        /// <summary>
-        /// 全てのプリキュアのデータを読み込む.
-        /// </summary>
-        /// <returns>プリキュアキーをキーとする<code>Dictionary</code>.</returns>
-        static Dictionary<string, Precure> LoadPrecures()
-        {
-            Dictionary<string, Precure> precures = new Dictionary<string, Precure>();
-            var rootPath = rubicureConfigPath + "girls/";
-            string[] files = Directory.GetFiles(rootPath, "*.yml", SearchOption.TopDirectoryOnly);
-            foreach (string file in files)
-            {
-                var input = new StreamReader(file, Encoding.UTF8);
-                var deserializer = new Deserializer();
-                Dictionary<string, Dictionary<string, object>> girls_tmp = deserializer.Deserialize<Dictionary<string, Dictionary<string, object>>>(input);
-                foreach (var girl in girls_tmp)
-                {
-                    if (girl.Value.Count == 1)
-                    {
-                        // エイリアスは飛ばす
-                        continue;
-                    }
-                    string girlName = girl.Key, precureName = null;
-                    if (girl.Value.TryGetValue("precure_name", out object precureNameObj))
-                    {
-                        precureName = (string)precureNameObj;
-                    }
-                    girlName = ToPascal(girlName);
-                    string initial = girlName.Replace("Cure ", "").Substring(0, 1);
-                    precures.Add(girl.Key, new Precure {
-                        GirlName = girlName,
-                        PrecureName = precureName,
-                        Initial = initial
-                    });
-                }
-            }
-            return precures;
-        }
-
-        /// <summary>
-        /// プリキュアリストを出力する.
-        /// </summary>
-        /// <param name="seriesName">シリーズ名</param>
-        /// <param name="precures">シリーズプリキュアキーリスト</param>
-        /// <param name="precureList">プリキュアリスト</param>
-        static void OutputPrecureList(string seriesName, List<string> precures, Dictionary<string, Precure> precureList)
-        {
-            Console.WriteLine(seriesName);
-            Console.WriteLine("プリキュア名,英語,頭文字");
-            // イニシャルリスト
-            List<string> precureInitials = new List<string>();
-            // イニシャル重複チェック
-            bool initialsConflict = false;
-            foreach (var precureKey in precures)
-            {
-                if (!precureList.TryGetValue(precureKey, out Precure precure))
-                {
-                    // 万が一取得できなかった場合はスキップする
-                    continue;
-                }
-                if (precureInitials.Contains(precure.Initial))
-                {
-                    initialsConflict = true;
-                }
-                precureInitials.Add(precure.Initial);
-                Console.WriteLine($"{precure.PrecureName},{precure.GirlName},{precure.Initial}");
-            }
-            Console.WriteLine($"人数：{precureInitials.Count}");
-            Console.WriteLine($"かぶり：{(initialsConflict ? "あり" : "なし")}");
-            Console.WriteLine();
-        }
-
-        /// <summary>
-        /// cure_black => Cure Blackのような変換
-        /// ref. https://increment-i.hateblo.jp/entry/csharp/regularexpression/pascal
-        /// </summary>
-        /// <param name="text">変換元</param>
-        /// <returns>変換結果</returns>
-        private static string ToPascal(string text)
-        {
-            return Regex.Replace(
-                text.Replace("_", " "),
-                @"\b[a-z]",
-                match => match.Value.ToUpper());
         }
     }
 }
